f0859ff0abce7716e366bb2f98e948c4e806475a
Tazi Aymane <taziaymane68@gmail.com>
2025-10-23 21:56:26 +0100

    DAY 2

--- bin/project.dart
+++ bin/project.dart
@@ -1,5 +1,19 @@
-void main(){
+ import 'dart:io';
 
-  var name = 'aymane';
-  print('hello m ');
+void main(){
+  //print("Enter your name");
+  //String? name = stdin.readLineSync();
+ // print("hello $name");
+// print("enter num1");
+// int? a = int.parse(stdin.readLineSync()!) ; 
+// print("Enter num2");
+// int? b = int.parse(stdin.readLineSync().toString()) ;
+// print("$a + $b = ${a + b} ") ;
+
+   String name = "aymane";
+  print(name.length);
+
 }
+
+
+
