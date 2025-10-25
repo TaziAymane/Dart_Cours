a3b6b918db424a563d28be9e8a21206ae5923ac3
Tazi Aymane <taziaymane68@gmail.com>
2025-10-25 21:25:18 +0100

    DAY 3

--- bin/project.dart
+++ bin/project.dart
@@ -43,8 +43,50 @@ void main(){
   //     print("invalid number of day");
   // }
 
-  int age = 32 ;
-  assert(age >0 , "your age must be >0"  );
-  print("your age is $age");
+  // int age = 32 ;
+  // assert(age >0 , "your age must be >0"  );
+  // print("your age is $age");
+  // for (int i = 1900; i <= 2025; i++) {
+  //   print(i);
+  // }
+  // int i = 1900 ;
+  // while (i < 2025) {
+  //   print(i);
+  //   i++ ;
+  // }
+
+  // int i = 1900 ;
+  // do {
+  //   print(i);
+  //   i++ ;
+  // } while (i < 1600);
+
+  // for(int i=0 ; i <= 10 ; i++){
+  //     print(i);
+  //     if (i == 5) {
+  //       print("we are in number 5");
+  //       // continue ;
+  //     }
+  // }
+  
+  List studants = ["aymane" , "karim" , "mohamed"];
+
+  // print(studants);
+  studants.insertAll(0,["AHMED" , "SAMIR"]);
+  // print(studants);
+
+  // for (var i = 0; i < studants.length; i++) {
+  //   print(studants[i]);
+  // }
+
+  // studants.forEach( (element){
+  //   print(element);
+  // }) ;
+
+  List numbres = [] ;
+  for (var i = 0; i <= 20; i++) {
+    numbres.add(i * 2);
+  }
+  print(numbres);
 }
 
