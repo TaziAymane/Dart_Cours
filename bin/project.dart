2a56e039e7a1b00c16d7cf5e9f763fdc2cf476d6
Tazi Aymane <taziaymane68@gmail.com>
2025-10-27 20:49:24 +0100

    DAY 4

--- bin/project.dart
+++ bin/project.dart
@@ -1,15 +1,11 @@
 import 'dart:io';
 
 void main() {
-  List<int> number = [12, 14, 15, 51, 54, 215, 1, 10, 65, 87, 11];
-  List<int> number2 = [];
-
-  number.forEach((item) {
-    if (item % 2 == 0) {
-      number2.add(item);
-    }
-  });
-
-  print(number2);
+  sum(15,10);
 }
+
+sum(int a,int b){
+  print(a + b);
+}
+
 
