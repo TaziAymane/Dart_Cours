6e2031da2c1b0064d106aab8b6660d4507c2b1aa
Tazi Aymane <taziaymane68@gmail.com>
2025-11-08 12:16:51 +0100

    OOP Static Variable

--- bin/project.dart
+++ bin/project.dart
@@ -1,44 +1,23 @@
 void main() {
-  Humane h1 = Humane() ;
-  h1.name = "aymane";
-  h1.identite = "SH321654";
-  h1.age = 21 ;
-  // h1.HumaneInformation();
-
-  Studant st1 = Studant();
-  st1.name = "Ahmed";
-  st1.identite = "B321654";
-  st1.age = 22;
-  st1.cin = "B132456";
-  st1.niveau = "Bac+3";
-  st1.moyyeneGeneral = 15.21 ;
-  st1.studantInformation();
+  Count count1 = Count();
+  count1.addCount();
+  count1.addCount();
+  count1.addCount();
+  count1.addCount();
+  count1.printCount();
+  Count count2 = Count();
+  count2.printCount();
 }  
 
-class Humane{
-  String? name ;
-  String? identite ;
-  int? age ;
-
-  humaneInformation(){
-    print("Name : $name \nIdentite : $identite \nAge : $age");
+class Count{
+  static int  count = 0 ;
+  printCount(){
+    print("count : $count");
   }
-
-  
+  addCount(){
+    count++ ;
+  }
 }
-
-class Studant extends Humane{
-  String? cin ;
-  String? niveau ;
-  double? moyyeneGeneral ;   
-
-   studantInformation(){
-    humaneInformation();
-    print("CIN : $cin \nNiveau : $niveau \nMoyyene General : $moyyeneGeneral");
-   }
-}
-
-
 
 
 
