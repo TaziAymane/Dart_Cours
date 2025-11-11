14f6c4ace9c6a42a0424f8529ab8fe66dce68b22
Tazi Aymane <taziaymane68@gmail.com>
2025-11-11 13:54:47 +0100

    OOP Abstract Class

--- bin/project.dart
+++ bin/project.dart
@@ -1,14 +1,27 @@
 void main() {
-  Studant.printname("aymane");
+  Studant s1 = Studant();
+  s1.name = "Ahmed";
+  s1.lastname = "asari";
+  s1.age = 21;
+  s1.cne = "B321654";
+  s1.niveau = "BAC+3";
+
+  s1.studantsnformation();
 }  
 
-class Studant {
-  static String Text = "Bac+2" ;
-  static printname(name){
-    print("Welcome $name $Text");
-  }
+abstract class Human {
+  String? name ;
+  String? lastname ;
+  int? age ;
 }
 
+class Studant extends Human {
+  String? cne ;
+  String? niveau ;
+  studantsnformation(){
+    print("$name \n$lastname \n$age \n$cne \n$niveau");
+  }
+}
 
 
 
