ff32e65b48f4037aa42abf6cc1366a491642756e
Tazi Aymane <taziaymane68@gmail.com>
2025-11-13 16:54:28 +0100

    OOP mixin ( with )

--- bin/project.dart
+++ bin/project.dart
@@ -1,27 +1,18 @@
 void main() {
-  Studant s1 = Studant();
-  s1.name = "Ahmed";
-  s1.lastname = "asari";
-  s1.age = 21;
-  s1.cne = "B321654";
-  s1.niveau = "BAC+3";
-
-  s1.studantsnformation();
+  Studant studant = Studant();
+  studant.printName();
 }  
 
-abstract class Human {
-  String? name ;
-  String? lastname ;
-  int? age ;
-}
-
-class Studant extends Human {
-  String? cne ;
-  String? niveau ;
-  studantsnformation(){
-    print("$name \n$lastname \n$age \n$cne \n$niveau");
+mixin mixinPrint{
+  printName(){
+    print("name");
   }
 }
+
+class Studant with mixinPrint {
+
+}
+
 
 
 
