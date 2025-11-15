4439e18e2e5f20babec985d04c22c698b25d3736
Tazi Aymane <taziaymane68@gmail.com>
2025-11-15 15:07:22 +0100

    Future & then

--- bin/project.dart
+++ bin/project.dart
@@ -1,13 +1,14 @@
-void  main() async{
-  print("one");
-  await Future.delayed(Duration(seconds: 3),(){
-    print('two');
-  });
-  print("three");
-  print("four");
+void  main() async {
+ await getdata().then((val){
+   print("welcom $val");
+ });
 }
 
-
+Future getdata() async {
+  return await Future.delayed(Duration(seconds: 3),(){
+    return "aymane";
+  });
+}
 
 
 
